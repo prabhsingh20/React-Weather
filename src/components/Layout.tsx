1d3fbbec0afb1e1de28d8e39b5b13446565a6fc1
Prabh ‪‪ <singh20prabh@gmail.com>
2025-02-17 12:26:23 +0530

    api checkpoints setup

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,4 @@
+import { Haze, SunSnow } from "lucide-react";
 import type { PropsWithChildren } from "react";
 import Header from "./Header";
 
@@ -8,9 +9,11 @@ export function Layout({ children }: PropsWithChildren) {
       <main className="min-h-screen container mx-auto px-4 py-8">
         {children}
       </main>
-      <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12">
-        <div className="container mx-auto px-4 text-center text-gray-200">
-          <p>Made with 💗 by RoadsideCoder</p>
+      <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-6">
+        <div className="container mx-auto px-4 text-center text-gray-200 flex  justify-center">
+          <p className="flex juscenter items-center gap-2">
+            <SunSnow /> React Weather <Haze />
+          </p>
         </div>
       </footer>
     </div>
