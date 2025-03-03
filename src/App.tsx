8ea930f0826bb93d140e29077b1c0b810fab9d04
Prabh ‪‪ <singh20prabh@gmail.com>
2025-03-03 16:31:02 +0530

    query client setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Layout } from "./components/Layout";
-import { ThemeProvider } from "./context/theme-provider";
-import CityPage from "./pages/CityPage";
-import WeatherDashboard from "./pages/WeatherDashboard";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "./components/ui/sonner";
+import { WeatherDashboard } from "./pages/WeatherDashboard";
+import { Layout } from "./components/layout";
+import { ThemeProvider } from "./context/theme-provider";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CityPage } from "./pages/city-page";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      gcTime: 10 * 60 * 1000, // 10 minutes
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
-const App = () => {
+function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
@@ -19,11 +29,12 @@ const App = () => {
               <Route path="/city/:cityName" element={<CityPage />} />
             </Routes>
           </Layout>
+          <Toaster richColors />
         </ThemeProvider>
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
-};
+}
 
 export default App;
