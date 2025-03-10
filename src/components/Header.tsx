5cb59e0753de5d2e1bdb3e39366d741c7a48dbca
Prabh ‪‪ <singh20prabh@gmail.com>
2025-03-10 22:15:39 +0530

    fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-2">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <Link to={"/"}>
-          <h1>React Weather</h1>
+          <h1> React Weather 🌦</h1>
         </Link>
 
         <div className="flex gap-4">
